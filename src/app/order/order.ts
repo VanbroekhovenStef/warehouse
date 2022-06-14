2389c796ecaca10baec50891bf05894c6ed899a4
VanbroekhovenStef <r0630975@student.thomasmore.be>
2022-06-14 19:03:43 +0200

    last update

--- src/app/order/order.ts
+++ src/app/order/order.ts
@@ -1,6 +1,9 @@
+import { Address } from "../address/address";
+
 export interface Order {
     id: number;
     addressId: number;
     userId: number;
     confirm: boolean;
+    address: Address;
 }
